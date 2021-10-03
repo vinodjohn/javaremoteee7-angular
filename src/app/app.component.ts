6a29f5f9d2bb67bcb0f1767425868c5551c4ae0b
vinodjohn <vinodjohn56@gmail.com>
2021-10-03 10:22:01 +0300

    Exercise tasks till 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,12 @@ import {Dog} from "./model/Dog";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'My first app!';
   doggies: Dog[] = [];
 
   ngOnInit(): void {
     this.doggies.push(new Dog('Jack', 'Dober', 'Run faster'));
-    this.doggies.push(new Dog('Julie', 'Alsheshian', 'Eats well'));
+    // this.doggies.push(new Dog('Julie', 'Alsheshian', 'Eats well'));
   }
 }
